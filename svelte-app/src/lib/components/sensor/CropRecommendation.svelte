af5924a40486380322d89f7fa03591abda195536
SiddharthSahani <sidhucroedde@gmail.com>
2025-04-22 22:55:27 +0530

    chore: api for recommendations

--- svelte-app/src/lib/components/sensor/CropRecommendation.svelte
+++ svelte-app/src/lib/components/sensor/CropRecommendation.svelte
@@ -1,9 +1,12 @@
 <script>
-    let someText = `\
-Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
+    import { onMount } from "svelte";
+    let someText = $state("Loading...");
 
-Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
-`;
+    onMount(async () => {
+        const resp = await fetch("/api/recommendations");
+        const respJson = await resp.json();
+        someText = respJson.data;
+    });
 </script>
 
 <div class="w-full rounded-lg bg-gray-200 p-8">
